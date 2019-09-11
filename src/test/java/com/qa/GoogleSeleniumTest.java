07e415dd119b00dcfc804b32b703410348d5b083
Sehher Tariq <sehher.tariq@academytrainee.com>
2019-09-11 10:04:42 +0100

    Added tests for opening a google page then searching funny cat pics

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -1,9 +1,46 @@
 package com.qa;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class GoogleSeleniumTest {
 
     private ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Profile\\IdeaProjects\\seleniumtesting\\src\\test\\java\\resources\\chromedriver.exe");
+
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void searchTest() throws InterruptedException {
+        // //maximises the screen
+        driver.get("http://google.com");
+        Thread.sleep(2000); //This keeps the window open for 2 seconds before closing it
+        WebElement searchField = driver.findElement(By.name("q"));
+        assertTrue(searchField.isDisplayed());
+        searchField.sendKeys("funny cat pictures"); //adds text to search field
+        Thread.sleep(5000);
+        WebElement submitButton = driver.findElement(By.name("btnK"));
+        submitButton.click();
+        Thread.sleep(5000);
+        WebElement linkToBiggerPicture = driver.findElementByLinkText("Images for funny cat"); //finds the link in the web search
+        linkToBiggerPicture.click();
+        Thread.sleep(5000);
+    }
 
 }
