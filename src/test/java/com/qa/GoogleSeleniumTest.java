8cd7294e6ec24333f492a1ca879e715f10cb1cf9
Sehher Tariq <sehher.tariq@academytrainee.com>
2019-09-11 15:44:53 +0100

    Added test for the selenium practice page (Submit form)

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -155,4 +155,50 @@ public class GoogleSeleniumTest {
         Thread.sleep(2000);
     }
 
+    @Test
+    public void inputFormTest() throws InterruptedException{
+        driver.get("https://www.seleniumeasy.com/test/input-form-demo.html");
+        Thread.sleep(2000);
+        WebElement fillFirstName = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[1]/div/div/input");
+        fillFirstName.sendKeys("Suvnah");
+        Thread.sleep(2000);
+        WebElement fillLastName = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[2]/div/div/input");
+        fillLastName.sendKeys("Goober");
+        Thread.sleep(2000);
+        WebElement fillEmail = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[3]/div/div/input");
+        fillEmail.sendKeys("suvnahisbetrayme@hotmail.com");
+        Thread.sleep(2000);
+        WebElement fillNumber = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[4]/div/div/input");
+        fillNumber.sendKeys("0161");
+        Thread.sleep(2000);
+        WebElement fillAddress = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[5]/div/div/input");
+        fillAddress.sendKeys("1 Betrayer Street, Betrayerville");
+        Thread.sleep(2000);
+        WebElement fillCity = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[6]/div/div/input");
+        fillCity.sendKeys("Traitorcester");
+        Thread.sleep(2000);
+        WebElement fillState = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[7]/div/div/select");
+        fillState.click();
+        Thread.sleep(2000);
+        WebElement fillStateOption = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[7]/div/div/select/option[5]");
+        fillStateOption.click();
+        Thread.sleep(2000);
+        WebElement fillZip = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[8]/div/div/input");
+        fillZip.sendKeys("blah23");
+        Thread.sleep(2000);
+        WebElement fillWeb = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[9]/div/div/input");
+        fillWeb.sendKeys("blah23.com");
+        Thread.sleep(2000);
+        WebElement fillHosting = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[10]/div/div[1]/label/input");
+        fillHosting.click();
+        Thread.sleep(2000);
+        WebElement Des = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[11]/div/div/textarea");
+        fillWeb.sendKeys("blah23.com");
+        Thread.sleep(2000);
+        WebElement clickSend = driver.findElementByXPath("//*[@id=\"contact_form\"]/fieldset/div[11]/div/div/textarea");
+        clickSend.click();
+        Thread.sleep(2000);
+
+    }
+
 }
